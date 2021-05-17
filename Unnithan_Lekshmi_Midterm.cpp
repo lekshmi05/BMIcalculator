1249590ace8e2aa85737f4f731251faf5490c5e7
lekshmi05 <84352790+lekshmi05@users.noreply.github.com>
2021-05-17 17:17:25 -0400

    Add files via upload

---
+++ Unnithan_Lekshmi_Midterm.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? downpayment;
+   ?? discountifany;
+   ?? carprice;
+   ?? otherfees;
+   ?? loanamount;
+   ?? tradeinvalue;
+
+   raptor_prompt_variable_zzyz =Enterthecarprice;
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> carPrice;
+   raptor_prompt_variable_zzyz =Enterthedownpayment;
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> downPayment;
+   raptor_prompt_variable_zzyz =TradeInvalue;
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> tradeInValue;
+   raptor_prompt_variable_zzyz =OtherFees;
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> otherFees;
+   raptor_prompt_variable_zzyz =Discounts;
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> discountIfAny;
+   loanAmount =(carPrice+otherFees)-(downPayment+tradeInValue+discountsIfAny);
+
+   return 0;
+}
